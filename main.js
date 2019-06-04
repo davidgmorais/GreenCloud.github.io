cf54869f54761ed67a7daebf440fe33a9efdbfdc
David Morais <49589844+davidgmorais@users.noreply.github.com>
2019-06-04 12:05:50 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,10 @@
-var options= ko.observableArray([
-    {"name": "Book a trip", "url": "./booking/booking.html"},
-    {"name": "Add a location", "url": "booking.html"},
-    {"name": "Become a member", "url": "booking.html"},
-    {"name": "Talk to others", "url": "./chat/chat.html"},
-    {"name": "Client support", "url": "booking.html"},
-    {"name": "About our investor", "url": "booking.html"}
-]);
-
 $(document).ready( function() {
 
     let login = localStorage.getItem("Login");
     let name = localStorage.getItem("name");
+    let partner = localStorage.getItem("partner");
 
     console.log(name);
-
-    ko.applyBindings(options);
-
-
 
     setInterval(function(){
 		$("#arrow").fadeOut()
@@ -57,5 +45,65 @@ $(document).ready( function() {
         window.location = "./index.html"
     });
 
+    $("#addLocationForm").click( function() {
+        if (login != 1) {
+            window.location = "./login/login.html"
+        }
+
+        if (partner != "true") {
+
+            $("#notPartner").show();
+            $("#FormBody").hide();
+            $("#msg").hide();
+            $("#submitForm").hide()
+
+
+        } else {
+
+            $("#notPartner").hide();
+            $("#FormBody").show();
+            $("#msg").hide();
+            $("#submitForm").show()
+
+        }
+
+        $("#adress").val("");
+        $("#city").val("");
+        $("#opinion").val("");
+        $("#pics").val("");
+
+    })
+
+    $("#submitForm").click( function() {
+        $("#notPartner").hide();
+        $("#FormBody").hide();
+        $("#msg").show();
+        $("#submitForm").hide()
+    })
+
+
+    $("#bmForm").click( function() {
+        if (login != 1) {
+            window.location = "./login/login.html"
+        }
+
+        $("#FormBody2").show();
+        $("#msg2").hide();
+        $("#submitForm2").show()
+
+
+        $("#name").val("");
+        $("#nr").val("");
+        $("#opinion").val("");
+        $("#file").val("");
+
+    })
   
+    $("#submitForm2").click( function() {
+        $("#FormBody2").hide();
+        $("#msg2").show();
+        $("#submitForm2").hide()
+    })
+
 });
+
