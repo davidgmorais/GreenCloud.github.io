48bbe4fd6eb7ab8f3df3f4c65f736865e4bcb4df
David Morais <49589844+davidgmorais@users.noreply.github.com>
2019-06-07 00:38:00 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-$(document).ready( function() {
+function formatAMPM(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
+}
+
+function resetChat() {
+    $("ul").empty();
+}
+
+$(document).ready(function () {
+
+    //-- Clear Chat
+    resetChat();
 
     let login = localStorage.getItem("Login");
     let name = localStorage.getItem("name");
@@ -6,46 +24,46 @@ $(document).ready( function() {
 
     console.log(name);
 
-    setInterval(function(){
-		$("#arrow").fadeOut()
-		$("#arrow").fadeIn()
+    setInterval(function () {
+        $("#arrow").fadeOut()
+        $("#arrow").fadeIn()
 
-	},1000)
+    }, 1000)
 
 
 
-    $("#arrow_box").click( function() {
+    $("#arrow_box").click(function () {
         $('html, body').animate({
-			scrollTop: $(window).height()
-		}, 1200);
-		
+            scrollTop: $(window).height()
+        }, 1200);
+
     })
 
     if (login == 1) {
         console.log("your already logged in");
         $("#login").addClass("dropdown-toggle");
-        $("#login").attr("data-toggle","dropdown");
+        $("#login").attr("data-toggle", "dropdown");
         $("#login").text(localStorage.getItem("name"));
     }
 
-    $("#login").click(function() {
+    $("#login").click(function () {
         if (login != 1) {
             window.location = "./login/login.html";
         }
     });
 
-    $("#logout").click( function() {
+    $("#logout").click(function () {
         console.log("loging out...");
         $("#login").removeClass("dropdown-toggle");
         $("#login").removeAttr("data-toggle");
-        localStorage.setItem("Login",0);
-        localStorage.setItem("name",null);
-        localStorage.setItem("lname",null);
-        localStorage.setItem("email",null);
+        localStorage.setItem("Login", 0);
+        localStorage.setItem("name", null);
+        localStorage.setItem("lname", null);
+        localStorage.setItem("email", null);
         window.location = "./index.html"
     });
 
-    $("#addLocationForm").click( function() {
+    $("#addLocationForm").click(function () {
         if (login != 1) {
             window.location = "./login/login.html"
         }
@@ -74,7 +92,7 @@ $(document).ready( function() {
 
     })
 
-    $("#submitForm").click( function() {
+    $("#submitForm").click(function () {
         $("#notPartner").hide();
         $("#FormBody").hide();
         $("#msg").show();
@@ -82,7 +100,7 @@ $(document).ready( function() {
     })
 
 
-    $("#bmForm").click( function() {
+    $("#bmForm").click(function () {
         if (login != 1) {
             window.location = "./login/login.html"
         }
@@ -98,12 +116,47 @@ $(document).ready( function() {
         $("#file").val("");
 
     })
-  
-    $("#submitForm2").click( function() {
+
+    $("#submitForm2").click(function () {
         $("#FormBody2").hide();
         $("#msg2").show();
         $("#submitForm2").hide()
     })
+
+
+    function insertChat(who, text) {
+        var control = "";
+        var date = formatAMPM(new Date());
+
+        control = "<li>" + who + " @ " + date + ": " + text + "</li>";
+
+        $("ul").append(control);
+
+    }
+
+    $("#mytext").on("keyup", function (e) {
+        if (e.which == 13) {
+            var text = $(this).val();
+            if (text !== "") {
+                insertChat("me", text);
+                $(this).val('');
+            }
+        }
+    });
+
+    $("#send").click(function () {
+        var text = $("#mytext").val();
+        if (text !== "") {
+            insertChat("me", text);
+            $("#mytext").val('');
+        }
+
+    });
+
+    $(".info").click( function(){
+        window.location = "./info/info.html"
+    })
+
 
 });
 
